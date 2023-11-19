b1a1df69d2ec7f7a04b858e2a8451378e57b4b39
vsc <vsc@DESKTOP-OIPKR0P>
2023-11-19 20:19:32 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿List<double> grades = new List<double>() { 12.5, 10.3, 2.8, 10.4, 2 };
+grades.Add(14.3);
+foreach (var grade in grades)
+{
+    Console.WriteLine(grade);
+}
+Console.WriteLine("----------");
+Console.WriteLine(grades[0]);
---
+++ Week45MiniProject/Program.cs
@@ -0,0 +1,48 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("My Products");
+        List<Product> prod = new List<Product>();
+        
+        while (true)
+        {
+            Console.WriteLine("Enter product category and press enter.");
+            string category = Console.ReadLine();
+            if (category.ToLower().Trim() == "q")
+            {
+                break;
+            }
+
+            Console.WriteLine("Enter product name and press enter.");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter price and press enter.");
+            int price = Convert.ToInt32(Console.ReadLine());
+            
+            prod.Add(new Product(category, name, price));
+        }
+        Console.WriteLine("");
+        foreach (Product prods in prod)
+        {
+            
+            Console.WriteLine(prods.Category + " " + prods.Name + " " + prods.Price);
+            
+        }
+        Console.WriteLine("");
+        Console.WriteLine("----------------------------");
+    }
+}
+
+class Product
+{
+    public Product(string category, string name, int price)
+    {
+        Category = category;
+        Name = name;
+        Price = price;
+    }
+
+    public string Category { get; set; }
+    public string Name { get; set; }
+    public int Price { get; set; }  
+}

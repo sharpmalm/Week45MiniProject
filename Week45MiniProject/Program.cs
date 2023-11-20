57f8f0822c34a3c425c5027d2fd56c2bf1b41e40
sharpmalm <no@example.com>
2023-11-20 15:11:49 +0100

    Program code wrapped in switch statement

--- Week45MiniProject/Program.cs
+++ Week45MiniProject/Program.cs
@@ -1,45 +1,65 @@
-﻿class Program
+﻿
+class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("My Products");
         List<Product> prod = new List<Product>();
-                
-        while (true)
+        bool exit = false;
+        while (!exit)
         {
-            Console.WriteLine("Enter product category and press enter.");
-            string category = Console.ReadLine();
-            if (category.ToLower().Trim() == "q")
+            Console.WriteLine("Enter 1 to add a product, 2 to display the list of products, 3 to search for a product, or 4 to exit.");
+            string input = Console.ReadLine();
+            switch (input)
             {
-                break;
+                case "1":
+                    Console.WriteLine("Enter product category and press enter.");
+                    string category = Console.ReadLine();
+                    Console.WriteLine("Enter product name and press enter.");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Enter price and press enter.");
+                    int price = Convert.ToInt32(Console.ReadLine());
+                    prod.Add(new Product(category, name, price));
+                    break;
+                case "2":
+                    List<Product> sortedList = prod.OrderBy(prod => prod.Price).ToList();
+                    Console.WriteLine("");
+                    Console.WriteLine("Category".PadRight(20) + "Name".PadRight(20) + "Price");
+                    foreach (Product product in sortedList)
+                    { Console.WriteLine(product.Category.PadRight(20) + product.Name.PadRight(20) + product.Price); }
+                    Console.WriteLine("");
+                    int totalPrice = prod.Sum(prod => prod.Price);
+                    Console.WriteLine("The total price for your products is:   " + totalPrice);
+                    break;
+                case "3":
+                    Console.WriteLine("Enter product name to search for and press enter.");
+                    string searchName = Console.ReadLine();
+                    List<Product> searchResults = prod.Where(prod => prod.Name.ToLower().Contains(searchName.ToLower())).ToList();
+                    if (searchResults.Count > 0)
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Category".PadRight(20) + "Name".PadRight(20) + "Price");
+
+
+                        foreach (Product product in searchResults)
+                        { Console.WriteLine(product.Category.PadRight(20) + product.Name.PadRight(20) + product.Price); }
+                        Console.WriteLine("");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No products found with that name.");
+                    }
+                    break;
+                case "4":
+                    exit = true;
+                    break;
+                default:
+                    Console.WriteLine("Invalid input. Please try again.");
+                    break;
             }
-
-            Console.WriteLine("Enter product name and press enter.");
-            string name = Console.ReadLine();
-            Console.WriteLine("Enter price and press enter.");
-            int price = Convert.ToInt32(Console.ReadLine());
-            
-            prod.Add(new Product(category, name, price));
         }
-        List<Product> sortedList = prod.OrderBy(prod => prod.Price).ToList();
-        Console.WriteLine("");
-        Console.WriteLine("Category".PadRight(20) + "Name".PadRight(20) + "Price");
-        
-        //foreach (Product prods in prod)
-        foreach (Product product in sortedList)
-        {
-
-            //Console.WriteLine(prods.Category.PadRight(20) + prods.Name.PadRight(20) + prods.Price);
-            Console.WriteLine(product.Category.PadRight(20) + product.Name.PadRight(20) + product.Price);
-        }
-        
-        Console.WriteLine("");
-        int totalPrice = prod.Sum(prod => prod.Price);
-        Console.WriteLine("The total price for your products is:   " + totalPrice);
-        Console.WriteLine();
-        Console.WriteLine("----------------------------");
     }
 }
+
 
 class Product
 {
@@ -49,8 +69,7 @@ class Product
         Name = name;
         Price = price;
     }
-
     public string Category { get; set; }
     public string Name { get; set; }
-    public int Price { get; set; }  
+    public int Price { get; set; }
 }
